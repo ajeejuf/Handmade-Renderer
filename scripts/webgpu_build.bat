f665cd205d4563823b5be62417ed0d3dcd1d5ae6
ajeejuf <alejand.fernand@ufl.edu>
2024-10-15 14:24:11 -0400

    removed glfw binaries

--- scripts/webgpu_build.bat
+++ scripts/webgpu_build.bat
@@ -1,31 +1,22 @@
 @echo off
 
-setlocal enabledelayedexpansion
-
 set application_name=wgpu_test
 set build_options=
 set lib_dir=..\lib
 set inc_dir=..\include
 
-set includes=/I%lib_dir%\dawn\include /I%lib_dir%\dawn\out\Debug\gen\include
-set libs=/LIBPATH:%lib_dir%\dawn\out\Debug
+set includes=/I%lib_dir%\dawn\install\Release\include /I%inc_dir% /I..\src /I%inc_dir%\glfw\include\
+set libs=/LIBPATH:%lib_dir%\dawn\install\Release\lib /LIBPATH:%lib_dir%\glfw
 
 set compiler_flags=/nologo /Zi /FAsc /EHsc /wd4312
-set linker_flags=/opt:ref /incremental:no dawn_proc.dll.lib dawn_native.dll.lib dawn_wire.dll.lib webgpu_dawn.dll.lib
+set linker_flags=/opt:ref /incremental:no webgpu_dawn.lib
 
 if not exist ..\build mkdir ..\build
 pushd ..\build
 
-set dawn_dll_dir=%lib_dir%\dawn\out\Debug
-for %%f in ("%dawn_dll_dir%\*.dll") do (
-	copy "%%f" .\
-)
+copy %lib_dir%\glfw\glfw3.dll .\
+copy %lib_dir%\dawn\install\Release\bin\webgpu_dawn.dll .\
 
-for %%f in ("%dawn_dll_dir%\*.lib") do (
-	set linker_flags=!linker_flags! "%%~nxf"
-	echo "!linker_flags!"
-)
-
-cl %compiler_flags% %includes% ..\src\webgpu\main.c /link  %libs% !linker_flags! /out:%application_name%.exe
+cl %compiler_flags% %includes% ..\src\webgpu\main.c /link  %libs% %linker_flags% /out:%application_name%.exe
 
 popd
